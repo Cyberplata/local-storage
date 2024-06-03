2c958a3ec4e1260ee60505091e1b9a4292d4475f
PlatonKynin <kininplaton@gmail.com>
2024-06-03 18:50:06 +0300

    2.Add fn clearLocalStorage and removeItemFromLocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,26 @@ function App() {
         setValue(value + 1);
     }
 
-    const setToLocalStorage = () => {
+    const setToLocalStorage = () => { // Устанавливает значение в local storage
         localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
     }
 
     const getFromLocalStorage = () => { //Получаем значение-Value, которое есть в local storage в браузере
-        debugger
         let valueAsString = localStorage.getItem('counterValue')
         if (valueAsString) {
             let newValue = JSON.parse(valueAsString)
             setValue(newValue)
         }
+    }
+
+    const clearLocalStorage = () => { // Зачищаем local storage + UI {value}
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeItemFromLocalStorage = () => { // Зачищает определённый значение, которое указали (в нашем случае 2ой вариант, где value + 1)
+        localStorage.removeItem('counterValue + 1')
     }
 
     return (
@@ -28,6 +37,8 @@ function App() {
             <button onClick={incHandler}>inc</button>
             <button onClick={setToLocalStorage}>setToLocalStorage</button>
             <button onClick={getFromLocalStorage}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorage}>clearLocalStorage</button>
+            <button onClick={removeItemFromLocalStorage}>removeItemFromLocalStorage</button>
         </div>
     );
 }
